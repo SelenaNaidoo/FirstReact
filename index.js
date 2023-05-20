dab972c317ffc470b247441f3e135fb52945c5a7
Selena <selena.naidoo18@gmail.com>
2023-05-20 19:18:15 -0400

    New files

---
+++ index.js
@@ -0,0 +1,8 @@
+ReactDOM.render(<h1>Hello Everyone</h1>, document.getElementById("root"))
+
+ReactDom.render(
+    <ul>
+        <li>Tomatoes</li>
+        <li>Potatoes</li>
+    </ul>
+    ,document.getElementById("root  "))
